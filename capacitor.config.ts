631da683007c0ea6b8df48f900fcf5477b7518d6
thejuliohd <moranjulio78@gmail.com>
2024-03-05 16:10:19 -0700

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'DianosticPc',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
